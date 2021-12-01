aa2ef4534723dfc96d3c183eec609042e0cc5da3
Swagnik Dutta <swagnikd@gmail.com>
2021-12-01 18:47:20 +0530

    day 1, p2

--- d1/main.go
+++ d1/main.go
@@ -8,6 +8,14 @@ import (
 	"strconv"
 )
 
+func getsum(queue []int) int {
+	sum := 0
+	for _, v := range queue {
+		sum += v
+	}
+	return sum
+}
+
 func p1() {
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -31,7 +39,40 @@ func p1() {
 }
 
 func p2() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
+	scanner := bufio.NewScanner(file)
+	var queue []int
+	firstTripletComplete := false
+	sum := 0
+	count := 0
+
+	for scanner.Scan() {
+		number, _ := strconv.Atoi(scanner.Text())
+
+		if firstTripletComplete {
+			queue = queue[1:]
+			queue = append(queue, number)
+			newsum := getsum(queue)
+
+			if newsum > sum {
+				count += 1
+			}
+			sum = newsum
+		} else {
+			queue = append(queue, number)
+
+			if len(queue) == 3 {
+				firstTripletComplete = true
+				sum = getsum(queue)
+			}
+		}
+	}
+	fmt.Println(count)
 }
 
 func main() {
